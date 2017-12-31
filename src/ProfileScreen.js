5f8f70b8c22e378735ff861a87fc1863d46eb8fc
mengnans <thundersmn@outlook.com>
2017-12-31 14:02:34 +1100

    profilescreen update

--- src/ProfileScreen.js
+++ src/ProfileScreen.js
@@ -4,24 +4,12 @@ import {
 } from 'react-native';
 
 
-export default class ProfileScreen extends ChosenScreen {
-
-    render() {
-        return (
-            <ProfileScreen>
-                <Text>Profile</Text>
-            </ProfileScreen>
-        );
-    }
-}
-
-
-class ChosenScreen extends Component {
+export default class ProfileScreen extends Component {
 
     render() {
         return (
             <View style={{flex:1, backgroundColor:this.props.bgColor}}>
-                {this.props.children}
+                <Text>Profile</Text>
             </View>
         );
     }
